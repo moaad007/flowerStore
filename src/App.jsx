b2920543f48e8fdd6739ad4614ee90551bec9408
moaad007 <147991720+moaad007@users.noreply.github.com>
2025-04-06 16:08:26 +0200

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from '../public/components/Nav';
+function App() {
+ 
+
+  return (
+    <>
+    <div className='bg-[url(assets/background.png)] bg-cover bg-center h-screen'>
+      <Nav/>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
