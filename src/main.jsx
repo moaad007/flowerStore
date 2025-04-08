65eb59d18e6ed1bb43efbdf75c69737b4737ae76
moaad007 <147991720+moaad007@users.noreply.github.com>
2025-04-08 13:30:10 +0200

    BASE CHANGE

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider,createHashRouter } from 'react-router-dom';
+import { RouterProvider,createHashRouter } from 'react-router-dom';
 import './index.css';
 import Home from './pages/home';
 import About from './pages/about';
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'/flowerStore',
+  base: process.eventNames.VITE_BASE_PATH ||'/flowerStore',
 })
