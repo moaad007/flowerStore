77e0f441b2f3fe52f1c7e4f2d070da2cab22722d
moaad007 <147991720+moaad007@users.noreply.github.com>
2025-04-08 03:55:21 +0200

    routs fixed

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,10 +6,10 @@ function Nav() {
             <div className="logo flex place-content-between bg-transparent text-black p-4">
                 <h1 className="text-3xl font-bold">Flower Store</h1>
             <ul className="nav-list flex justify-center items-center bg-transparent text-black p-4 gap-10">
-                <Link to={'./'}> <li className="nav-item">Home</li>  </Link>
-                <Link to={'./about'}> <li className="nav-item">About</li> </Link>
-                <Link to={'./products'}> <li className="nav-item">Products</li>  </Link>
-                <Link to={'./contact'}>  <li className="nav-item">Contact</li> </Link>
+                <Link to={'/'}> <li className="nav-item">Home</li>  </Link>
+                <Link to={'/about'}> <li className="nav-item">About</li> </Link>
+                <Link to={'/products'}> <li className="nav-item">Products</li>  </Link>
+                <Link to={'/contact'}>  <li className="nav-item">Contact</li> </Link>
             </ul>
             </div>
         </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider,createHashRouter } from 'react-router-dom';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
@@ -9,14 +9,14 @@ import Products from './pages/products';
 import ErrorPage from './pages/errorpage';
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
 
     { path: "/", element: <Home /> },
     { path: "/about", element: <About /> },
     { path: "/contact", element: <Contact /> },
     { path: "/products", element: <Products /> },
     { path: "*", element: <ErrorPage /> }
-  ], { basename: "/flowerStore" });
+  ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
